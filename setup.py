07f54627e11c275e29f6f59baa9311fda720aea6
Saadha Salim <saadha.salim1@gmail.com>
2025-02-21 13:55:47 +0530

    gitignore update and version num

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 
 setup(
     name="distributed_imm",
-    version="0.2.1",
+    version="0.3.2",
     description="A distributed implementation of Iterative Mistake Minimization (IMM) for clustering explanations",
     author="Saadha",
     author_email="marium.20@cse.mrt.ac.lk",
